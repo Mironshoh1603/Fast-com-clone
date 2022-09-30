dd554eeb8f15079154530d675423bb1e7a3acd53
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-09-30 19:10:05 +0500

    backend is prepared

--- client/src/utils/apiEndpoints.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = "http://localhost:2000";
-export const GET_INTERNET_SPEED = "/api/speed-test";
--- client/src/utils/apiRequests.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getRequest = async (url) => {
-  const data = await axios
-    .get(url)
-    .then((resp) => resp.data)
-    .catch((err) => ({ error: err.response.data }));
-  return data;
-};
